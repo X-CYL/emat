f18bb6f14b7d9ea1c94ba7fff34f7f01798fcb08
X-CYL <castellastro@gmail.com>
2024-02-19 22:25:21 +0100

    styled the dropdown menu, creating routes, menu icons

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,10 +2,11 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-             ("/but", "Présentation","/pages/but.html"),
-             ("/connexion", "Connexion","/pages/connexion.html"),
-             ("/pages/interface", "Interface appli","/pages/interface.html")];
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/but", "Présentation", "/pages/but.html"),
+  new Route("/connexion", "Connexion", "/pages/connexion.html"),
+  new Route("/interface", "Interface appli", "/pages/interface.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "emat-documentation";
